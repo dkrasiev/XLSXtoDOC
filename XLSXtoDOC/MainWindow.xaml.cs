a7a7468403f4d07a01b6ce18cc683b9c1a22d179
dmitr <dmitr@VR19018>
2021-09-16 13:40:07 +0400

    Добавьте файлы проекта.

---
+++ XLSXtoDOC/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace XLSXtoDOC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            Program program = new Program();
+
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Filter = "Excel документ (*.xlsx)|*.xlsx|Все файлы (*.*)|*.*",
+                CheckFileExists = true
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                if (program.LoadXLSX(dlg.FileName).Length > 0)
+                {
+                    textBoxLoadedFile.Text = program.LoadXLSX(dlg.FileName);
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XLSXtoDOC/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace XLSXtoDOC
+{
+    class Program
+    {
+        public string LoadXLSX(string filename)
+        {
+            string result = "";
+
+            string FileName = @"C:\Users\bee\Downloads\test.xlsx";
+            object rOnly = true;
+            object SaveChanges = false;
+            object MissingObj = System.Reflection.Missing.Value;
+
+            Excel.Application app = new Excel.Application();
+            Excel.Workbooks workbooks = null;
+            Excel.Workbook workbook = null;
+            Excel.Sheets sheets = null;
+            try
+            {
+                workbooks = app.Workbooks;
+                workbook = workbooks.Open(FileName, MissingObj, rOnly, MissingObj, MissingObj,
+                                            MissingObj, MissingObj, MissingObj, MissingObj, MissingObj,
+                                            MissingObj, MissingObj, MissingObj, MissingObj, MissingObj);
+
+                // Получение всех страниц докуента
+                sheets = workbook.Sheets;
+
+                foreach (Excel.Worksheet worksheet in sheets)
+                {
+                    // Получаем диапазон используемых на странице ячеек
+                    Excel.Range UsedRange = worksheet.UsedRange;
+                    // Получаем строки в используемом диапазоне
+                    Excel.Range urRows = UsedRange.Rows;
+                    // Получаем столбцы в используемом диапазоне
+                    Excel.Range urColums = UsedRange.Columns;
+
+                    // Количества строк и столбцов
+                    int RowsCount = urRows.Count;
+                    int ColumnsCount = urColums.Count;
+                    for (int i = 1; i <= RowsCount; i++)
+                    {
+                        for (int j = 1; j <= ColumnsCount; j++)
+                        {
+                            Excel.Range CellRange = UsedRange.Cells[i, j];
+                            // Получение текста ячейки
+                            string CellText = (CellRange == null || CellRange.Value2 == null) ? null :
+                                                (CellRange as Excel.Range).Value2.ToString();
+
+                            if (CellText != null)
+                            {
+                                result += CellText;
+                            }
+                        }
+                    }
+                    // Очистка неуправляемых ресурсов на каждой итерации
+                    if (urRows != null) Marshal.ReleaseComObject(urRows);
+                    if (urColums != null) Marshal.ReleaseComObject(urColums);
+                    if (UsedRange != null) Marshal.ReleaseComObject(UsedRange);
+                    if (worksheet != null) Marshal.ReleaseComObject(worksheet);
+                }
+            }
+            catch (Exception ex)
+            {
+                /* Обработка исключений */
+            }
+            finally
+            {
+                /* Очистка оставшихся неуправляемых ресурсов */
+                if (sheets != null) Marshal.ReleaseComObject(sheets);
+                if (workbook != null)
+                {
+                    workbook.Close(SaveChanges);
+                    Marshal.ReleaseComObject(workbook);
+                    workbook = null;
+                }
+
+                if (workbooks != null)
+                {
+                    workbooks.Close();
+                    Marshal.ReleaseComObject(workbooks);
+                    workbooks = null;
+                }
+                if (app != null)
+                {
+                    app.Quit();
+                    Marshal.ReleaseComObject(app);
+                    app = null;
+                }
+            }
+
+            return result;
+        }
+
+        public void SaveDOC(string filename)
+        {
+
+        }
+    }
+}
